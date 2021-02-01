055d7c3fb5c5a55bd889bb807fe6d61e5913dad1
thisvalera <valleerroon4ikSho@gmail.com>
2021-02-01 17:59:45 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,31 @@
-$(function(){
+$(function () {
 
-    
-$('.top-slider__inner').slick({
-    dots:true,
-    fade:true,
-    arrows:false,
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+$('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+$('.filter-price__from').text(data.from);
+$('.filter-price__to').text(data.to);
+
+    },
+
+  });
+
+
+  $('.top-slider__inner').slick({
+    dots: true,
+    fade: true,
+    arrows: false,
     autoplay: true,
     autoplaySpead: 2000,
 
-});
+  });
 
-$(".star").rateYo({
+  $(".star").rateYo({
     starWidth: "17px",
     normalFill: "#ccccce",
     ratedFill: "#ffc35b",
@@ -22,7 +37,7 @@ $(".star").rateYo({
     const minutes = Math.floor((total / 1000 / 60) % 60);
     const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
     const days = Math.floor(total / (1000 * 60 * 60 * 24));
-    
+
     return {
       total,
       days,
@@ -31,32 +46,32 @@ $(".star").rateYo({
       seconds
     };
   }
-  
+
   function initializeClock(id, endtime) {
     const clock = document.querySelector('.promo__clock');
     const daysSpan = clock.querySelector('.promo__days');
     const hoursSpan = clock.querySelector('.promo__hours');
     const minutesSpan = clock.querySelector('.promo__minutes');
     const secondsSpan = clock.querySelector('.promo__seconds');
-  
+
     function updateClock() {
       const t = getTimeRemaining(endtime);
-  
+
       daysSpan.innerHTML = t.days;
       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
+
       if (t.total <= 0) {
         clearInterval(timeinterval);
       }
     }
-  
+
     updateClock();
     const timeinterval = setInterval(updateClock, 1000);
   }
-  
+
   const deadline = $('.promo__clock').attr('data-time');
   initializeClock('promo__clock', deadline);
- 
+
 });
